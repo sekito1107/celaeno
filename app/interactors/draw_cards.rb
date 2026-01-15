522fcf132d911930437fe66d93438cb323ca6885
sekito <sekito1107gg@gmail.com>
2026-01-15 18:57:15 +0900

    デッキ切れでの敗北をデッキが0枚になった時から、0枚でドローしようとした時に変更

--- app/interactors/draw_cards.rb
+++ app/interactors/draw_cards.rb
@@ -8,9 +8,13 @@ class DrawCards
     return if game.finished?
 
     game.game_players.order(:id).each do |player|
-      player.draw_card!
-      game.check_deck_death!(player)
-      break if game.reload.finished?
+      drawn_card = player.draw_card!
+
+      # デッキが空で引けなかった場合に敗北
+      if drawn_card.nil?
+        game.finish_deck_death!(player)
+        break
+      end
     end
   end
 end
