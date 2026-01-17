27e9e8aace3a9e33a1e578a113eb5a4b33204ee0
sekito <sekito1107gg@gmail.com>
2026-01-17 10:43:42 +0900

    フィールドコンポーネントのN+1クエリ対策

--- app/components/game/field_component.rb
+++ app/components/game/field_component.rb
@@ -6,11 +6,18 @@ class Game::FieldComponent < ApplicationComponent
   end
 
   def deck_count
-    @game_player.deck.count
+    # N+1対策: メモリ上の game_cards からカウント
+    @game_player.game_cards.count { |gc| gc.location_deck? }
   end
 
   def graveyard_top_card
-    @game_player.graveyard.last
+    # N+1対策: メモリ上の game_cards から検索 (updated_at, id でソートされている前提だが、game_cardsは順不同の可能性があるためソートが必要)
+    # しかし、大量のカードがある場合 sort は重い。ここでは簡易的に id 最大を取得するか、
+    # コントローラーでの incldes 時に order を指定するのがベストだが、
+    # view_component 内での sort は許容範囲とする。
+    @game_player.game_cards
+                .select { |gc| gc.location_graveyard? }
+                .max_by { |gc| [ gc.updated_at, gc.id ] }
   end
 
   def slot_card(position)
