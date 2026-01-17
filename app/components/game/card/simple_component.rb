4857b50a08741acfa46af0b408aa4409f0fcdf4c
sekito <sekito1107gg@gmail.com>
2026-01-17 17:39:20 +0900

    CardComponentのサブコンポーネント（Simple, Detail）の実装

---
+++ app/components/game/card/simple_component.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Game::Card::SimpleComponent < Game::Card::BaseComponent
+      def initialize(card_entity:, variant: :hand)
+        super(card_entity: card_entity)
+        @variant = variant
+      end
+      
+      def variant_field?
+        @variant == :field
+      end
+      
+      def interactive?
+        # Only allow pinning/interaction if card is in hand or board
+        return false unless game_card?
+        
+        @card_entity.location_hand? || @card_entity.location_board?
+      end
+      
+      def detail_html
+        render(Game::Card::DetailComponent.new(card_entity: @card_entity)).to_str
+      end
+      
+      def card_hover_id
+        @card_entity&.id || 'preview'
+      end
+end
