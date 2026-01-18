c20fb0bf20f819b130463f257180e78d70a408bd
sekito <sekito1107gg@gmail.com>
2026-01-18 18:45:11 +0900

    fix: 自分のカード以外へのアクセス時に適切なエラーを返すよう修正

--- app/controllers/card_plays_controller.rb
+++ app/controllers/card_plays_controller.rb
@@ -3,7 +3,13 @@ class CardPlaysController < ApplicationController
   include GameActionHelper
 
   def create
-    game_card = @game_player.game_cards.find(params[:game_card_id])
+    game_card = @game_player.game_cards.find_by(id: params[:game_card_id])
+    unless game_card
+      return handle_game_action(
+        GameActionResult.failure("指定されたカードが見つかりません"),
+        status: :not_found
+      )
+    end
 
     result = PlayCard.call(
       game: @game,
