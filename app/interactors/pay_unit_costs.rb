b709052dddc3f3306e855dcc2a130019c83ec3f4
sekito <sekito1107gg@gmail.com>
2026-01-19 16:04:59 +0900

    堅牢性向上: PayUnitCostsにゲーム終了判定を追加

--- app/interactors/pay_unit_costs.rb
+++ app/interactors/pay_unit_costs.rb
@@ -2,6 +2,8 @@ class PayUnitCosts
   include Interactor
 
   def call
+    return if context.game.finished?
+
     PayResolvePhaseCosts.call(context.to_h.merge(target_card_types: [ :unit ]))
   end
 end
